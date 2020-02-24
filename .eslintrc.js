5c889cb8235ef90ad326f5a3043bd469736cf307
dhmmasson <dhmmasson+github@gmail.com>
2020-02-24 16:19:46 +0100

    Modify linter to use babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "node":true
   },
   "extends":"eslint:recommended",
+    "parser": "babel-eslint",
   "globals":{
     "Atomics":"readonly",
     "SharedArrayBuffer":"readonly",
