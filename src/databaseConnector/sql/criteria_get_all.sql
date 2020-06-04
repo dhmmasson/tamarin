7a1418b758e5ad88f41b4c0cc856dd08893c530c
dhmmasson <dhm.masson+github@gmail.com>
2020-06-04 12:31:29 +0200

    [FIX] Add criteria description
    Fix #16

--- src/databaseConnector/sql/criteria_get_all.sql
+++ src/databaseConnector/sql/criteria_get_all.sql
@@ -1,6 +1,7 @@
 SELECT criteria.name
      , min( data.value ) AS min
      , max( data.value ) AS max
+     , description 
 FROM criteria
   LEFT JOIN data ON data.criteria_name = criteria.name
 WHERE type_name = 'numeric'
