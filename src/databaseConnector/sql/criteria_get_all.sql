78ad9b797ac0daee427509c540e8e7f05b314f4d
dhmmasson <dhm.masson+github@gmail.com>
2020-09-30 12:17:57 +0200

    [FIX] Get criteria to take into account the sorting order in the group clause (#29)
    fix the file criteria_get_all.sql, i added the column to the group clause, it should always be the same value...

--- src/databaseConnector/sql/criteria_get_all.sql
+++ src/databaseConnector/sql/criteria_get_all.sql
@@ -1,11 +1,10 @@
 SELECT criteria.name
      , min( data.value ) AS min
      , max( data.value ) AS max
-     , criteria.description 
+     , criteria.description
 	 , sortingorder_name AS sortingorder
 FROM criteria
 	JOIN criteria_has_sortingorder ON criteria.name = criteria_has_sortingorder.criteria_name
-  LEFT JOIN data ON data.criteria_name = criteria.name 
+  LEFT JOIN data ON data.criteria_name = criteria.name
 WHERE type_name LIKE 'numeric'
-GROUP BY criteria.name 
-	
+GROUP BY criteria.name, sortingorder_name
