99d024026371448d965727443baa6a2c04c7cbcd
ESTIA\l.valade <l.valade@net.estia.fr>
2020-10-01 13:36:48 +0200

    [FIX] PWD in windows
    replace process.env.PWD by process.cwd() in two files, until the project start.
    Might cause problems as cwd might be different from PWD
    https://github.com/nodejs/help/issues/1881

--- app.mjs
+++ app.mjs
@@ -18,29 +18,29 @@ app.use( cookieParser() ) ;
 
 /** Use scss on demand */
 app.use( sassMiddleware(
-  { src            : path.join( process.env.PWD, "public" )
-  , dest           : path.join( process.env.PWD, "public" )
+  { src            : path.join( process.cwd(), "public" )
+  , dest           : path.join( process.cwd(), "public" )
   , indentedSyntax : false // .scss
   , sourceMap      : true
   } ) ) ;
 
-updateTemplate( path.resolve( process.env.PWD, "views/partials" )
-  , path.resolve( process.env.PWD, "public/javascripts" ) ) ;
+updateTemplate( path.resolve( process.cwd(), "views/partials" )
+  , path.resolve( process.cwd(), "public/javascripts" ) ) ;
 
 /** Static routes : css, images, and app js*/
-app.use( express.static( path.join( process.env.PWD, "public" ) ) ) ;
+app.use( express.static( path.join( process.cwd(), "public" ) ) ) ;
 
 /** Static routes : libraries */
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/jquery/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/materialize-css/dist/js" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/@svgdotjs/svg.draggable.js/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/@svgdotjs/svg.js/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/papaparse" ) ) ) ;
+app.use( "/javascripts", express.static( path.join( process.cwd(), "node_modules/jquery/dist" ) ) ) ;
+app.use( "/javascripts", express.static( path.join( process.cwd(), "node_modules/materialize-css/dist/js" ) ) ) ;
+app.use( "/javascripts", express.static( path.join( process.cwd(), "node_modules/@svgdotjs/svg.draggable.js/dist" ) ) ) ;
+app.use( "/javascripts", express.static( path.join( process.cwd(), "node_modules/@svgdotjs/svg.js/dist" ) ) ) ;
+app.use( "/javascripts", express.static( path.join( process.cwd(), "node_modules/papaparse" ) ) ) ;
 
-app.use( "/fonts", express.static( path.join( process.env.PWD, "node_modules/materialize-css/dist/fonts" ) ) ) ;
+app.use( "/fonts", express.static( path.join( process.cwd(), "node_modules/materialize-css/dist/fonts" ) ) ) ;
 
 /** Autoloading routes */
-app.set( "views", path.join( process.env.PWD, "views" ) ) ;
+app.set( "views", path.join( process.cwd(), "views" ) ) ;
 app.set( "view engine", "pug" ) ;
 
 
