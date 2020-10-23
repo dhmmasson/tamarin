22f75fe5708c66f2f6b7362cb74de1518a6ceb8c
ESTIA\l.valade <l.valade@net.estia.fr>
2020-10-23 10:42:27 +0200

    [FEATURE] track changes in blur or weight values

---
+++ public/javascripts/Logs.mjs
@@ -0,0 +1,26 @@
+import { Downloader } from "./Downloader.mjs" ;
+import { Log } from "./Log.mjs" ;
+
+ /**
+   * constructor - create a new log
+   *
+   * @param  {array} - 			
+   */
+class Logs{
+	constructor(){
+		this.data=[];	
+	}
+	 /**
+   * updateData - When a data is updated, the changes are added to data.
+   *
+   * @param  type     type of the change (blurIntensity or weight)
+   * @param  criteria name of the criteria changed
+   */
+	updateData (criteria, type){
+		this.data.push(new Log(criteria, type));
+		console.log(this.data);
+	}
+}
+
+export { Logs };
+
