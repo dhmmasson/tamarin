f21ebeb6e02a42be87562588c60e1683b86676a6
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2023-02-06 23:43:33 +0100

    [Fix] Remove sassMiddleware, update pkg

--- app.mjs
+++ app.mjs
@@ -1,55 +1,55 @@
-import { autoLocals, autoRoute, mysqlConnector, updateTemplate } from "./src/expressUtils/index.mjs" ;
-import cookieParser from "cookie-parser" ;
-import dotenv from "dotenv" ;
-import errorHandler from "./routes/_error.js" ;
-import express from "express" ;
-import logger from "morgan" ;
-import path from "path" ;
-import sassMiddleware from "node-sass-middleware" ;
+import { autoLocals, autoRoute, mysqlConnector, updateTemplate } from "./src/expressUtils/index.mjs";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import errorHandler from "./routes/_error.js";
+import express from "express";
+import logger from "morgan";
+import path from "path";
+//stimport sassMiddleware from "node-sass-middleware" ;
 
 
-dotenv.config() ;
-const app = express() ;
-if( app.get( "env" ) ) app.use( logger( "dev" ) ) ;
+dotenv.config();
+const app = express();
+if (app.get("env")) app.use(logger("dev"));
 
-app.use( express.json() ) ;
-app.use( express.urlencoded( { extended: false } ) ) ;
-app.use( cookieParser() ) ;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /** Use scss on demand */
-app.use( sassMiddleware(
-  { src            : path.join( process.env.PWD, "public" )
-  , dest           : path.join( process.env.PWD, "public" )
-  , indentedSyntax : false // .scss
-  , sourceMap      : true
-  } ) ) ;
+// app.use( sassMiddleware(
+//   { src            : path.join( process.env.PWD, "public" )
+//   , dest           : path.join( process.env.PWD, "public" )
+//   , indentedSyntax : false // .scss
+//   , sourceMap      : true
+//   } ) ) ;
 
-updateTemplate( path.resolve( process.env.PWD, "views/partials" )
-  , path.resolve( process.env.PWD, "public/javascripts" ) ) ;
+updateTemplate(path.resolve(process.env.PWD, "views/partials")
+  , path.resolve(process.env.PWD, "public/javascripts"));
 
 /** Static routes : css, images, and app js*/
-app.use( express.static( path.join( process.env.PWD, "public" ) ) ) ;
+app.use(express.static(path.join(process.env.PWD, "public")));
 
 /** Static routes : libraries */
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/jquery/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/materialize-css/dist/js" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/@svgdotjs/svg.draggable.js/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/@svgdotjs/svg.js/dist" ) ) ) ;
-app.use( "/javascripts", express.static( path.join( process.env.PWD, "node_modules/papaparse" ) ) ) ;
+app.use("/javascripts", express.static(path.join(process.env.PWD, "node_modules/jquery/dist")));
+app.use("/javascripts", express.static(path.join(process.env.PWD, "node_modules/materialize-css/dist/js")));
+app.use("/javascripts", express.static(path.join(process.env.PWD, "node_modules/@svgdotjs/svg.draggable.js/dist")));
+app.use("/javascripts", express.static(path.join(process.env.PWD, "node_modules/@svgdotjs/svg.js/dist")));
+app.use("/javascripts", express.static(path.join(process.env.PWD, "node_modules/papaparse")));
 
-app.use( "/fonts", express.static( path.join( process.env.PWD, "node_modules/materialize-css/dist/fonts" ) ) ) ;
+app.use("/fonts", express.static(path.join(process.env.PWD, "node_modules/materialize-css/dist/fonts")));
 
 /** Autoloading routes */
-app.set( "views", path.join( process.env.PWD, "views" ) ) ;
-app.set( "view engine", "pug" ) ;
+app.set("views", path.join(process.env.PWD, "views"));
+app.set("view engine", "pug");
 
 
-app.locals = autoLocals() ;
-app.use( autoRoute( ) ) ;
+app.locals = autoLocals();
+app.use(autoRoute());
 // create a database connector
-app.set( "connector", mysqlConnector() ) ;
+app.set("connector", mysqlConnector());
 
 /** Error handling */
-app.use( errorHandler ) ;
+app.use(errorHandler);
 
-export default app ;
+export default app;
